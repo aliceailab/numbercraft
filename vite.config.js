ad08a9cadc4cd056704c296c0dbdeab4d3b6bbcf
aliceailab <alicezhzhang@gmail.com>
2026-03-29 20:36:57 -0400

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/numbercraft/',
   build: {
     rollupOptions: {
       output: {
@@ -11,4 +12,3 @@ export default defineConfig({
     },
   },
 });
-
